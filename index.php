790ccaece5b985941f53b8f842538dc3c24e1ca1
xhirashima <xhirashima@gmail.com>
2012-04-05 17:16:47 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$test = 1;
+$output = $test + 2;
+
+echo $output;
+
+phpinfo();
+?>
